9b4413f8b530c2f067730ab6d028d95ac99b66a5
Charlie Wood <charliemowood@gmail.com>
2017-05-12 01:40:41 +0100

    Change Sqlrunner for heroku database

--- db/sqlrunner.rb
+++ db/sqlrunner.rb
@@ -1,14 +1,14 @@
 require('pg')
 
 class SqlRunner
-  # db = PG.connect({dbname: 'animal_shelter', host: 'localhost'})
+
   def SqlRunner.run(sql)
     begin
-      db = PG.connect({dbname: 'd7b7l1jrc1pecf',
-        host: 'ec2-23-21-224-199.compute-1.amazonaws.com',
+      db = PG.connect({dbname: 'dasdkdsaldkj',
+        host: 'ec2-1-1-1-1-1.compute-1.amazonaws.com',
         port: 5432,
-        user: 'rqxbuvaliluyjr',
-        password: 'bd8dac258d4820ba82056b89db735da976b6d349e8f76111f5a8c1416a32cc72'})
+        user: 'nsdlkdjalskjd',
+        password: 'sadlskjadlkjASDAD'})
       db.exec(sql)
     ensure
       db.close()
