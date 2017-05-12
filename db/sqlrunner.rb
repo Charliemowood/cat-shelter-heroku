c76d571c44487c55244e109f68d0947f597d7ae1
Charlie Wood <charliemowood@gmail.com>
2017-05-12 01:49:31 +0100

    Further changes to heroku app

--- db/sqlrunner.rb
+++ db/sqlrunner.rb
@@ -4,11 +4,11 @@ class SqlRunner
 
   def SqlRunner.run(sql)
     begin
-      db = PG.connect({dbname: 'dasdkdsaldkj',
-        host: 'ec2-1-1-1-1-1.compute-1.amazonaws.com',
+      db = PG.connect({dbname: 'd7ld8tpt7a7njp',
+        host: 'ec2-54-83-205-71.compute-1.amazonaws.com',
         port: 5432,
-        user: 'nsdlkdjalskjd',
-        password: 'sadlskjadlkjASDAD'})
+        user: 'bpgtjixeazkmcx',
+        password: '468f7e84fc7a0c90208d8e8d6fc249c8ec1f9b58c1a6f5db24a16e6c8e1ca478'})
       db.exec(sql)
     ensure
       db.close()
